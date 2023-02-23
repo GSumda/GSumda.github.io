0076a5258dbaca6983787e9846175339ace3c2ad
GSumda <georgesumda@gmail.com>
2023-02-23 21:13:16 +0000

    html code for title and image

---
+++ main.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dalai Lama</title>
+</head>
+<body>
+
+<h1 style="background-color: green;">Dalai Lama</h1>
+<p>This is a paragraph.</p>
+<img src="dalailama.jpg" alt="His holiness the 14th Dalai Lama of Tibet" width="100" height="100">
+
+</body>
+</html>
